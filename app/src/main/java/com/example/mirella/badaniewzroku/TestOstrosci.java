668f56ff138c8e992ec3bf3670a899974b780f2c
Mirella <mirella.u@onet.pl>
2018-03-21 15:15:06 +0100

    new start

--- app/src/main/java/com/example/mirella/badaniewzroku/TestOstrosci.java
+++ app/src/main/java/com/example/mirella/badaniewzroku/TestOstrosci.java
@@ -2,8 +2,17 @@ package com.example.mirella.badaniewzroku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TestOstrosci extends AppCompatActivity {
+
+    protected Button btn_up;
+    protected Button btn_down;
+    protected Button btn_left;
+    protected Button btn_right;
+    protected ImageView imageE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
