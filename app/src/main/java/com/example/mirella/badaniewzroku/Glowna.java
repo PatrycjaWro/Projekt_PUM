dc8ba7edd05cfa38fae021ee782e09c0fb215bae
Mirella <mirella.u@onet.pl>
2018-03-21 16:14:12 +0100

    przechodzenie do widokow

--- app/src/main/java/com/example/mirella/badaniewzroku/Glowna.java
+++ app/src/main/java/com/example/mirella/badaniewzroku/Glowna.java
@@ -1,13 +1,58 @@
 package com.example.mirella.badaniewzroku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Glowna extends AppCompatActivity {
+    public class Glowna extends AppCompatActivity {
 
-    @Override
+        protected Button testOstr_btn;
+        protected Button testKontrastu_btn;
+        protected Button testWidzenia_btn;
+        protected Button testOgolny_btn;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glowna);
-    }
+
+        testOstr_btn = (Button) findViewById(R.id.testOstr_btn);
+        testKontrastu_btn = (Button) findViewById(R.id.testKontrastu_btn);
+        testWidzenia_btn = (Button) findViewById(R.id.testWidzenia_btn);
+        testOgolny_btn = (Button) findViewById(R.id.testOgolny_btn);
+
+            testOstr_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Glowna.this, TestOstrosci.class);
+                    startActivity(intent);
+                }
+            });
+
+            testKontrastu_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Glowna.this, TestKontrastu.class);
+                    startActivity(intent);
+                }
+            });
+
+            testWidzenia_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Glowna.this, TestWidzeniaBarw.class);
+                    startActivity(intent);
+                }
+            });
+
+//            testOgolny_btn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent intent = new Intent(this, TestOstrosci.class);
+//                    startActivity(intent);
+//                }
+//            });
+        }
 }
