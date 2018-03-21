490fbc3a6c54383648fb6584eaa61276f39cb245
Mirella <mirella.u@onet.pl>
2018-03-21 15:39:00 +0100

    gotowe widoki?

--- app/src/main/java/com/example/mirella/badaniewzroku/TestOstrosci.java
+++ app/src/main/java/com/example/mirella/badaniewzroku/TestOstrosci.java
@@ -2,15 +2,15 @@ package com.example.mirella.badaniewzroku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class TestOstrosci extends AppCompatActivity {
 
-    protected Button btn_up;
-    protected Button btn_down;
-    protected Button btn_left;
-    protected Button btn_right;
+    protected ImageButton btn_up;
+    protected ImageButton btn_down;
+    protected ImageButton btn_left;
+    protected ImageButton btn_right;
     protected ImageView imageE;
 
 
@@ -18,5 +18,12 @@ public class TestOstrosci extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_ostrosci);
+
+        btn_down = (ImageButton) findViewById(R.id.btn_down);
+        btn_up = (ImageButton) findViewById(R.id.btn_up);
+        btn_right = (ImageButton) findViewById(R.id.btn_left);
+        btn_left = (ImageButton) findViewById(R.id.btn_right);
+        imageE = (ImageView) findViewById(R.id.imageE);
+
     }
 }
