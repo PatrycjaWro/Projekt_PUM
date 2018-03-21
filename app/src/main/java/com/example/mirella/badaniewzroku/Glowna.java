fee2f35b4845f3a3634049bed6bfefb111509cdc
PatrycjaWro <patrwro597@student.polsl.pl>
2018-03-21 14:28:51 +0100

    Pierwszy widok

---
+++ app/src/main/java/com/example/mirella/badaniewzroku/Glowna.java
@@ -0,0 +1,13 @@
+package com.example.mirella.badaniewzroku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Glowna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glowna);
+    }
+}
