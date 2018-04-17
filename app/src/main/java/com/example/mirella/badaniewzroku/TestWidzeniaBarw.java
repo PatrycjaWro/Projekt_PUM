c14d73a3f7497cc680b36aac4c448350a0ff6487
PatrycjaWro <patrwro597@student.polsl.pl>
2018-04-17 16:31:49 +0200

    Pobiera wartość z numberPicker

--- app/src/main/java/com/example/mirella/badaniewzroku/TestWidzeniaBarw.java
+++ app/src/main/java/com/example/mirella/badaniewzroku/TestWidzeniaBarw.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -15,6 +17,8 @@ public class TestWidzeniaBarw extends AppCompatActivity {
 
     protected Button btn_ready;
     protected ImageView imageD;
+    protected NumberPicker numberPicker;
+    protected TextView textView2;
     final Random rnd = new Random();
     int sum=0;
     int count=0;
@@ -29,6 +33,10 @@ public class TestWidzeniaBarw extends AppCompatActivity {
 
         btn_ready=(Button)findViewById(R.id.btn_ready);
         imageD=(ImageView)findViewById(R.id.imageD);
+        numberPicker=(NumberPicker)findViewById(R.id.numberPicker);
+        numberPicker.setMaxValue(100);
+        numberPicker.setMinValue(0);
+        textView2 = (TextView)findViewById(R.id.textView2);
 
         final int [] images_1 = {R.drawable.i_96,R.drawable.i_42,R.drawable.i_35,R.drawable.i_5,R.drawable.i_18};
         number_1=rnd.nextInt(4);
@@ -38,14 +46,21 @@ public class TestWidzeniaBarw extends AppCompatActivity {
 
         btn_ready.setOnClickListener(new View.OnClickListener() {
 
+
             @Override
             public void onClick(View view) {
+
+                int value=numberPicker.getValue();
+
+                textView2.setText(Integer.toString(value));
+
 
                 if (count>=0 && count <=5)
                 {
                     number_1=rnd.nextInt(4);
                     imageD.setImageResource(images_1[number_1]);
                     count+=1;
+
 
                 }
                 else if(count==6)
