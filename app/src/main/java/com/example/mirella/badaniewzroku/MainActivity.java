c2f6edcfb7cf50dad4c4b5adc392596470ec64d4
Mirella <mirella.u@onet.pl>
2018-05-16 15:09:15 +0200

    Poprawa czystosci kodu w klasach ContrastTest.java i SharpTest.java

--- app/src/main/java/com/example/mirella/badaniewzroku/MainActivity.java
+++ app/src/main/java/com/example/mirella/badaniewzroku/MainActivity.java
@@ -21,11 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        btn_ready = (Button) findViewById(R.id.btn_ready);
+        btn_ready = findViewById(R.id.btn_ready);
         name = findViewById(R.id.name);
-        surname = (EditText) findViewById(R.id.surname);
-        age = (EditText) findViewById(R.id.age);
+        surname = findViewById(R.id.surname);
+        age = findViewById(R.id.age);
 
         btn_ready = findViewById(R.id.btn_ready);
         name = findViewById(R.id.name);
@@ -44,11 +43,12 @@ public class MainActivity extends AppCompatActivity {
                     name.setHint("Wpisz swoje imię!");
                     surname.setHint("Wpisz swoje nazwisko!");
                     age.setHint("Wpisz swój wiek!");
+                    Intent intent = new Intent(MainActivity.this, MainView.class);
+                    startActivity(intent);
 
                 }
                 else {
-                    Intent intent = new Intent(MainActivity.this, MainView.class);
-                    startActivity(intent);
+
 
                 }
             }
